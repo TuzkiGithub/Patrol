2d176beeef7b59d322ca8f135d2f95365445611b
chenhui <chenhui@2043600393.com>
2020-11-05 08:33:02 +0800

    人员管理常量类

---
+++ PIIS-Common/src/main/java/tech/piis/common/constant/ManagmentConstants.java
@@ -0,0 +1,29 @@
+package tech.piis.common.constant;
+
+/**
+ * ClassName : ManagmentConstants
+ * Package : tech.piis.common.constant
+ * Description :
+ *  人员管理常量类
+ * @author : chenhui@xvco.com
+ */
+public class ManagmentConstants {
+    /**
+     * 巡视/巡察专职数量
+     *
+     */
+    public static final String FULL_NUM = "fullNum";
+    /**
+     * 巡视/巡察兼职数量
+     *
+     */
+    public static final String PART_NUM = "partNum";
+    /**
+     * 巡视/巡察专职兼职和
+     */
+    public static final String FULL_PART_SUM = "sum";
+    /**
+     * 巡视/巡察机构全称
+     */
+    public static final String PIIS_WHOLE_NAME = "wholeName";
+}
